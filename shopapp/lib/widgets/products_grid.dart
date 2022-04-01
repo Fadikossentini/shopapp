c5bcdb21912213fbc6d031bc4473196a300d8bc0
fedi <fadikossentini@insat.u-carthage.tn>
2022-04-01 13:54:55 +0100

    using consumer

--- shopapp/lib/widgets/products_grid.dart
+++ shopapp/lib/widgets/products_grid.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../models/product.dart';
+import '../providers/product.dart';
 import 'product_item.dart';
 import 'package:provider/provider.dart';
 import '../providers/products.dart';
@@ -12,10 +12,13 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ProductItem(
-        products[i].id,
-        products[i].title,
-        products[i].imageUrl,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        create: (ctx) => products[i],
+        child: ProductItem(
+            // products[i].id,
+            // products[i].title,
+            // products[i].imageUrl,
+            ),
       ),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
