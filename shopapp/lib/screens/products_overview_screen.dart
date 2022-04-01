ce97da29c66e44c84df571ae7e3828debd851da9
fedi <fadikossentini@insat.u-carthage.tn>
2022-04-01 17:42:24 +0100

    fixin errors

--- shopapp/lib/screens/products_overview_screen.dart
+++ shopapp/lib/screens/products_overview_screen.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopapp/providers/products.dart';
 import '../widgets/products_grid.dart';
 
-class ProductsOverViewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorite,
+  All,
+}
+
+class ProductsOverViewScreen extends StatefulWidget {
+  @override
+  State<ProductsOverViewScreen> createState() => _ProductsOverViewScreenState();
+}
+
+class _ProductsOverViewScreenState extends State<ProductsOverViewScreen> {
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("MyShop"),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorite) {
+                productsContainer.showFavoritesOnly();
+              } else {
+                productsContainer.showAll();
+              }
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                  child: Text('Only Favourites'),
+                  value: FilterOptions.Favorite),
+              PopupMenuItem(child: Text('Show all'), value: FilterOptions.All),
+            ],
+          )
+        ],
       ),
       body: ProductsGrid(),
     );
--- shopapp/lib/widgets/product_item.dart
+++ shopapp/lib/widgets/product_item.dart
@@ -11,7 +11,7 @@ class ProductItem extends StatelessWidget {
   // ProductItem(this.id, this.title, this.imageUrl);
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final Product product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -29,14 +29,16 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+              onPressed: () {
+                product.toggleFavouriteStatus();
+              },
+              color: Theme.of(context).accentColor,
             ),
-            onPressed: () {
-              product.toggleFavouriteStatus();
-            },
-            color: Theme.of(context).accentColor,
           ),
           title: Text(
             product.title,
